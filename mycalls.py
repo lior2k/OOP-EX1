3c64b6048883832d1b7524d7519da78cd4e24754
lior2k <92747945+lior2k@users.noreply.github.com>
2021-11-13 20:05:22 +0200

    Update mycalls.py

--- mycalls.py
+++ mycalls.py
@@ -11,3 +11,12 @@ class MyCalls:
 
     def __str__(self):
         return self.list_of_calls.__str__()
+
+    def get_next_index(self, call):
+        counter=0
+        for i in self.list_of_calls:
+            if i.__eq__(call):
+                return counter+1
+            else:
+                counter += 1
+        return -1

9f7e581fe924301034da821440f2c2fb5c38809c
lior2k <92747945+lior2k@users.noreply.github.com>
2021-11-19 01:50:17 +0200

    Update mycalls.py

--- mycalls.py
+++ mycalls.py
@@ -1,9 +1,13 @@
 from mycall import MyCall
 
+
 class MyCalls:
 
-    def __init__(self):
+    def __init__(self, list: list):
         self.list_of_calls = []
+        for x in list:
+            temp_call = MyCall(x[1], x[2], x[3])
+            self.list_of_calls.append(temp_call)
 
     def __add__(self, other):
         if isinstance(other, MyCall):
@@ -12,11 +16,21 @@ class MyCalls:
     def __str__(self):
         return self.list_of_calls.__str__()
 
+    def get_calls_list(self):
+        return self.list_of_calls
+
     def get_next_index(self, call):
-        counter=0
+        counter = 0
         for i in self.list_of_calls:
             if i.__eq__(call):
                 return counter+1
             else:
                 counter += 1
         return -1
+
+    def __iter__(self):
+        return iter(self.list_of_calls)
+
+    def get_next_call(self, call):
+        i = self.get_next_index(call)
+        return self.list_of_calls[i]
